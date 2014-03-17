7c0a6c52d6bb5bdd9eac6ef40dfe67d526d863bc
alicjasalamon <alicjasalamon@gmail.com>
2014-03-17 20:59:05 +0100

    calendar fixed?

--- public/js/hourPicker.js
+++
@@ -1,9 +0,0 @@
-//$(document).ready(function(){
-   $.mask.definitions['h'] = "([012])";
-   $.mask.definitions['m'] = "([012345])";
-   $("#hour-picker").mask("h9:m9");
-   //alert("ziom");
- //});
- 
- //to mi sie bardzo nie podoba i mysle zeby zrobic takie:
- //http://jdewit.github.io/bootstrap-timepicker/
