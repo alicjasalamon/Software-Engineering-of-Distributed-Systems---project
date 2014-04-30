af9f0f32b3fe23f27ae1682df545bb6102aa7dee
kamilmielnik <kamil.adam.mielnik@gmail.com>
2014-05-01 01:50:30 +0200

    forgot a single line

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -7,6 +7,8 @@ use Zend\View\Model\ViewModel;
 class IndexController extends BaseController {
     
     public function indexAction() {
+        $this->requireAuth();
+        
         $viewModel = new ViewModel();
         $viewModel->setVariable('zmienna', 'Hello World!');
         return $viewModel;
