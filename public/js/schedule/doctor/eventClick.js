e2bedac72a87c82de96fea6e75c5a16b96f4f942
alicjasalamon <alicjasalamon@gmail.com>
2014-05-13 14:09:50 +0200

    do u like colors?

--- public/js/schedule/doctor/eventClick.js
+++ public/js/schedule/doctor/eventClick.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $('body').on('click', '.event', function(e) {
 
         var target = $(e.currentTarget);
-        var isEmpty = (target.children().length === 0)
-        var isMeasurement = target.hasClass('measurements');
+        var isEmpty = (target.children().length === 0);
+        var isMeasurement = target.parent().is('#measurementsStream');
         
         $.Dialog({
             overlay: true,
@@ -17,6 +17,7 @@ $(document).ready(function() {
                 var content = _dialog.children('.content');
                 var dialog = $('#doctorEventDialog');
                 
+                measurementSettings(isMeasurement, dialog);
                    
                 var newContent = $(dialog).html();
                 content.html(newContent);
@@ -29,4 +30,13 @@ $(document).ready(function() {
 function basicInformationFill()
 {
     
+}
+
+function measurementSettings(isMeasurement, dialog)
+{
+    var div = dialog.find('#measurementsDiv');
+    if(isMeasurement)   
+        div.show();
+    else
+        div.hide();
 }
